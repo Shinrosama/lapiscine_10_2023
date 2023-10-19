ac6087139a67b28b18b26a9729a780a10e0f102e
hamid Hazzab <hamid.hazzab@lapiscine.pro>
2023-10-19 14:54:19 +0200

    add replace lesson formateur

--- cours/index.js
+++ cours/index.js
@@ -1,75 +1,89 @@
-// présentation des opérateurs aritmétiques
-
-
+// Présentation des opérateurs arithmétiques
 
 /*
-commentaires en plusieurs lignes
+    Commentaires sur plusieurs lignes
 */
 
-//--------les nombres
+/**
+ * Variante de commentaires sur plusieurs lignes
+ */
+
+// LES NOMBRES
 
 var myNumber1 = 9 + 2
 var myNumber2 = 4
 
+myNumber2 = 6
+
 var myResult = myNumber1 * myNumber2
 
-//console.log(myNumber1,myNumber2,myResult)
+// console.log(myResult)
 
 
+// Déclaration moderne (en ES6, javascript depuis 2015) de variables
 
-//declaration moderne (en ES6, javascript depuis 2015) de variables
+const myNb1 = 3
+let myNb2 = 4
+myNb2++
 
-const mynb1 = 9
-let mynb2 = 4
+// console.log(myNb2)
 
+const res2 = myNb1 * myNb2
 
-mynb2 = 6
+// console.log(res2)
 
-const res = mynb1 * mynb2
-
-//console.log(res)
-
-
-//LES CHAINES DE CARACTERES 5 (string) ""  '' ``
-
-let myName = `Je m'appelle Shinro`
-
-const lastname = `Shinn`
-
-let mySpace = ` `
-
-const age = 30;
-
-let modifieage = age + 5
+// LES CHAINES DE CARACTERES (string) "" '' ``
+let myName = `Je m'appelle "Paul"`
+let mySpace = ' '
+const age = 35;
+let modifiedAge = age + 5
+const lastname = `Doazan`
 
 let myName2 = myName + mySpace + lastname
+myName2 += ', ' + modifiedAge + ' ans.'
 
-myName += `, ` + lastname
+const resultStr = `${myName} et j'ai ${modifiedAge} ans.`
 
-
-
-const resultStr = `${myName} et j'ai ${modifieage} ans.`
-
-
-//console.log(resultStr)
-
-
+// console.log(resultStr)
 
 
 // LES BOOLEENS
-
-const myboolean1 = true
-const myboolean2 = false
+const myBoolean1 = true
+const myBoolean2 = false
 
 let myNb = 5
 
-if (myboolean2) {
-
-    myNb += 7
-   
+if (myBoolean2) {
+    console.log('Je suis passé dans ma condition')
 }
 
-console.log(myNb)
+let myAge = 32
+const legalAge = 18
+
+const isMinor = myAge >= legalAge
+// const isMinor = myAge <= legalAge
+
+const is30yearsOld = myAge == 30
+
+if (isMinor) {
+    // console.log('Paul est mineur')
+}
+
+if (!isMinor) {
+    // console.log(`Paul n'est pas mineur`)
+}
+
+if (isMinor) {
+    // console.log('Paul est mineur')
+} else {
+    // console.log(`Paul n'est pas mineur`)
+}
+
+if (is30yearsOld) {
+    console.log('Paul a 30 ans')
+} else {
+    console.log(`Paul n'a pas 30 ans`)
+}
 
 
 
