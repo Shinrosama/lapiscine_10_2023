19a347a4e45ce8080fd5cc3f3811e2dec30a7a10
hamid Hazzab <hamid.hazzab@lapiscine.pro>
2023-10-19 14:51:01 +0200

    "add replace with lesson formateur"

---
+++ cours/index.js
@@ -0,0 +1,78 @@
+// présentation des opérateurs aritmétiques
+
+
+
+/*
+commentaires en plusieurs lignes
+*/
+
+//--------les nombres
+
+var myNumber1 = 9 + 2
+var myNumber2 = 4
+
+var myResult = myNumber1 * myNumber2
+
+//console.log(myNumber1,myNumber2,myResult)
+
+
+
+//declaration moderne (en ES6, javascript depuis 2015) de variables
+
+const mynb1 = 9
+let mynb2 = 4
+
+
+mynb2 = 6
+
+const res = mynb1 * mynb2
+
+//console.log(res)
+
+
+//LES CHAINES DE CARACTERES 5 (string) ""  '' ``
+
+let myName = `Je m'appelle Shinro`
+
+const lastname = `Shinn`
+
+let mySpace = ` `
+
+const age = 30;
+
+let modifieage = age + 5
+
+let myName2 = myName + mySpace + lastname
+
+myName += `, ` + lastname
+
+
+
+const resultStr = `${myName} et j'ai ${modifieage} ans.`
+
+
+//console.log(resultStr)
+
+
+
+
+// LES BOOLEENS
+
+const myboolean1 = true
+const myboolean2 = false
+
+let myNb = 5
+
+if (myboolean2) {
+
+    myNb += 7
+   
+}
+
+console.log(myNb)
+
+
+
+
+
+
